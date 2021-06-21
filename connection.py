704c1ded060fdd82c212548a7cd2399e72adf47d
Lukasz <l.wisniewski@student.po.edu.pl>
2021-06-21 02:41:23 +0200

    .

--- connection.py
+++ connection.py
@@ -2,5 +2,5 @@ import cx_Oracle
 def pol():
     cx_Oracle.init_oracle_client(lib_dir=  r"C:\oracle\instantclient_19_10")
     dsn = cx_Oracle.makedsn("217.173.198.135", 1522, sid="orcltp")
-    connection = cx_Oracle.connect("s97804", "s97804", dsn, encoding="UTF-8")
+    connection = cx_Oracle.connect("login", "haslo", dsn, encoding="UTF-8")
     return connection
